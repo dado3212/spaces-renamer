ad49b7278f17c1ab5cc125803e51cbacad8f3271
Alex Beals <alexcbeals@gmail.com>
2017-10-17 22:03:09 -0400

    Not working on WVDisplaySpaces

--- message-indicator/spacesRenamer.m
+++ message-indicator/spacesRenamer.m
@@ -10,44 +10,6 @@
 #import "ZKSwizzle.h"
 #import <QuartzCore/QuartzCore.h>
 
-// Hooks into the ChatTableCellView and hijacks the layouts to add the indicator
-ZKSwizzleInterface(custom_space, WAWindow, NSObject)
-@implementation custom_space
-+ (id)windowWithCGSWindow:(unsigned int)arg1 allowAttached:(BOOL)arg2 {
-    return nil;
-}
-+ (id)windowWithCGSWindow:(unsigned int)arg1 {
-    return nil;
-}
-+ (id)windowsWithOwner:(unsigned int)arg1 tags:(int [2])arg2 clearTags:(int [2])arg3 allowMinimized:(BOOL)arg4 {
-    return nil;
-}
-+ (id)windowsWithOwner:(unsigned int)arg1 tags:(int [2])arg2 clearTags:(int [2])arg3 allowMinimized:(BOOL)arg4 orderedIn:(BOOL)arg5 {
-    return nil;
-}
-+ (id)windowsForSwitcher:(unsigned int)arg1 {
-    return nil;
-}
-+ (void)setMenubarAlpha:(float)arg1 {
-    return;
-}
-+ (id)workspaceWindowsWithOwner:(unsigned int)arg1 tags:(int [2])arg2 clearTags:(int [2])arg3 space:(unsigned int)arg4 options:(int)arg5 {
-    return nil;
-}
-+ (id)windowsObscuringDesktop:(BOOL)arg1 {
-    return nil;
-}
-+ (id)hiddenWindows:(unsigned int)arg1 tags:(int [2])arg2 clearTags:(int [2])arg3 allowMinimized:(BOOL)arg4 {
-    return nil;
-}
-- (id)initWithCGSWindow:(unsigned int)arg1 type:(unsigned char)arg2 {
-    return nil;
-}
-- (id)initWithLocalWindow:(unsigned int)arg1 {
-    return nil;
-}
-
-//- (id)initWithSpace:(id)arg1 title:(id)arg2 andWindows:(id)arg3 usingScaleFactor:(float)arg4 {
 //    NSAlert *alert = [[NSAlert alloc] init];
 //    [alert addButtonWithTitle:@"OK"];
 //    [alert addButtonWithTitle:@"Cancel"];
@@ -57,10 +19,84 @@ ZKSwizzleInterface(custom_space, WAWindow, NSObject)
 //
 //    if ([alert runModal] == NSAlertFirstButtonReturn) {
 //        // OK clicked, delete the record
-//       // [self deleteRecord:currentRec];
+//        // [self deleteRecord:currentRec];
 //    }
 //    [alert release];
-//
-//    return nil; // ZKSuper(id, arg1, arg2, arg3, arg4);
+
+@interface WAWindow : NSObject
+@property(readonly, nonatomic) NSArray *windows; // @dynamic windows;
+@property(readonly, nonatomic) NSArray *descendents; // @dynamic descendents;
+@end
+
+ZKSwizzleInterface(custom_expose, WVDisplaySpaces, NSObject)
+@implementation custom_expose
+//- (_Bool)switchToSpace:(id)arg1 fromServer:(BOOL)arg2 updatePSN:(BOOL)arg3 {
+//    return ZKSuper(BOOL, arg1, arg2, arg3);
 //}
+//- (_Bool)switchToNextSpaceForApplication:(struct CPSProcessSerNum)arg1;
+//- (void)switchForWindowDrag:(_Bool)arg1;
+//- (_Bool)switchToNextSpace:(_Bool)arg1;
+//- (_Bool)switchToPreviousSpace:(_Bool)arg1;
+//- (void)switchToLastSpace;
+//- (_Bool)canAddUserSpace;
+- (void)insertSpace:(id)arg1 afterSpace:(id)arg2  {
+    NSString *str = [arg1 description]; //Your text or XML
+    [str writeToFile:@"/Users/alexbeals/Desktop/log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    ZKOrig(void, arg1, arg2);
+}
+- (void)moveSpace:(id)arg1 afterSpace:(id)arg2  {
+    NSString *str = [arg1 description]; //Your text or XML
+    [str writeToFile:@"/Users/alexbeals/Desktop/log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    ZKOrig(void, arg1, arg2);
+}
+- (void)removeSpace:(id)arg1  {
+    NSString *str = [arg1 description]; //Your text or XML
+    [str writeToFile:@"/Users/alexbeals/Desktop/log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    ZKOrig(void, arg1);
+}
+- (void)setCurrentSpace:(id)arg1  {
+    NSString *str = [arg1 description]; //Your text or XML
+    [str writeToFile:@"/Users/alexbeals/Desktop/log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    ZKOrig(void, arg1);
+}
+- (void)addUserSpace:(id)arg1  {
+    NSString *str = [arg1 description]; //Your text or XML
+    [str writeToFile:@"/Users/alexbeals/Desktop/log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    ZKOrig(void, arg1);
+}
+- (void)insertSpace:(id)arg1 atIndex:(unsigned long long)arg2  {
+    NSString *str = [arg1 description]; //Your text or XML
+    [str writeToFile:@"/Users/alexbeals/Desktop/log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    ZKOrig(void, arg1, arg2);
+}
+- (void)addSpace:(id)arg1 {
+    NSString *str = [arg1 description]; //Your text or XML
+    [str writeToFile:@"/Users/alexbeals/Desktop/log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    ZKOrig(void, arg1);
+}
+@end
+
+// Hooks into the ChatTableCellView and hijacks the layouts to add the indicator
+ZKSwizzleInterface(custom_space, WAWindow, NSObject)
+@implementation custom_space
+- (void)updateFrame {
+
+//    NSString *str = [((WAWindow *)self) description]; //Your text or XML
+//    [str writeToFile:@"/Users/alexbeals/Desktop/log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+
+
+//    [alert addButtonWithTitle:@"OK"];
+//    [alert addButtonWithTitle:@"Cancel"];
+//    [alert setMessageText:@"Delete the record?"];
+//    [alert setInformativeText:@"Deleted records cannot be restored."];
+//    [alert setAlertStyle:NSWarningAlertStyle];
+//
+//    if ([alert runModal] == NSAlertFirstButtonReturn) {
+//        // OK clicked, delete the record
+//        // [self deleteRecord:currentRec];
+//    }
+    // [alert release];
+
+    ZKOrig(void);
+}
 @end
